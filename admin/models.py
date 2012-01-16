b94d8e477a13b6ca7ba1c726713be5ae061fe0d8
florinjurca <florinjurca@reea.net>
2012-01-16 12:51:34 +0200

    first commit

---
+++ admin/models.py
@@ -0,0 +1,108 @@
+import datetime,urllib
+from django.db import models
+from django.utils.encoding import smart_str
+from django.utils.hashcompat import md5_constructor, sha_constructor
+from django.forms import ModelForm
+
+# Create your models here.
+
+
+def get_hexdigest(algorithm, salt, raw_password):
+    """
+    Returns a string of the hexdigest of the given plaintext password and salt
+    using the given algorithm ('md5', 'sha1' or 'crypt').
+    """
+    raw_password, salt = smart_str(raw_password), smart_str(salt)
+    if algorithm == 'crypt':
+        try:
+            import crypt
+        except ImportError:
+            raise ValueError('"crypt" password algorithm not supported in this environment')
+        return crypt.crypt(raw_password, salt)
+    
+    if algorithm == 'md5':
+        return md5_constructor(salt + raw_password).hexdigest()
+    elif algorithm == 'sha1':
+        return sha_constructor(salt + raw_password).hexdigest()
+    raise ValueError("Got unknown password algorithm type in password.")
+
+
+class TestUsersAuth(models.Model):
+    '''
+    Test users table
+    '''
+    id = models.AutoField(primary_key=True, editable=False)
+    username = models.CharField('Username', max_length=30, unique=True)
+    password = models.CharField('Password', max_length=128)
+    first_name = models.CharField('First name', max_length=30, blank=True)
+    last_name = models.CharField('Last name', max_length=30, blank=True)
+    email = models.EmailField('E-mail address', blank=True)
+    is_admin = models.BooleanField('is admin', default=False)
+    cr_date = models.DateTimeField('creation date', default=datetime.datetime.now, editable=False)
+    
+    def set_password(self, raw_password):
+        if raw_password is None:
+            self.set_unusable_password()
+        else:
+            import random
+            algo = 'sha1'
+            salt = get_hexdigest(algo, str(random.random()), str(random.random()))[:5]
+            hsh = get_hexdigest(algo, salt, raw_password)
+            self.password = '%s$%s$%s' % (algo, salt, hsh)
+
+
+class TestUsersAuthForm(ModelForm):
+    class Meta:
+        model = TestUsersAuth
+            
+
+
+class TestUsersManager(models.Manager):
+    '''
+    User manager 
+    '''
+    def create_user(self, username, email, password):
+        '''
+        Create an user 
+        '''
+        now = datetime.datetime.now()
+        
+        try:
+            email_name, domain_part = email.strip().split('@', 1)
+        except ValueError:
+            pass
+        else:
+            email = '@'.join([email_name, domain_part.lower()])
+            
+        user = self.model(username=username, email=email, is_admin=False, cr_date=now)
+        
+        user.set_password(password)
+        
+        user.save(using=self._db)
+        
+    
+    def edit_user(self,email,password,first_name='',last_name=''):
+        '''
+        Change the user profile
+        '''
+        
+        try:
+            email_name, domain_part = email.strip().split('@', 1)
+        except ValueError:
+            pass
+        else:
+            email = '@'.join([email_name, domain_part.lower()])
+        
+        user = self.model(email=email,first_name=first_name,last_name=last_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+    
+    def delete_user(self,username):
+        '''
+        Delete the user
+        '''
+        user = self.model(username=username)
+        user.delete()
+        
+    
---
+++ admin/views.py
@@ -0,0 +1,90 @@
+# Create your views here.
+
+from django.template import RequestContext,loader
+#from django.template import Context,loader #if we use shortcuts there is no need for this import
+from django.views.decorators.csrf import csrf_protect #
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect #we have no redirects on server side 
+from django.core.urlresolvers import reverse #we have no redirects here on server side 
+from admin.models import *
+
+
+def index(request,id=None):
+    '''
+    The view of users-list window
+    '''
+    
+    if id!=None:
+        id = int(id)
+    
+    if id>0:
+        user = TestUsersAuth.objects.get(pk=id)
+        user.delete()
+        return HttpResponseRedirect('/admin/')
+        
+    users_list = TestUsersAuth.objects.all() #.order_by('cr_date')
+    
+    #By default render_to_response uses Context TEMPLATE_CONTEXT_PROCESSORS
+    return render_to_response('admin/users_list.html',{'users_list':users_list})
+    
+
+#@csrf_protect
+def edit(request,id,save=None):
+    '''
+    The view of user-edit popup 
+        -load the initial view containing the form and csrf token
+            -the form may be empty when id==0
+            -the form may be filled when id>0
+        -display the view containing the form csrf token and errors
+            -the form will be filled and id>0
+        -redirect the output to index page
+    '''
+    
+    id = int(id)
+    user = None
+    if save==None:
+        if id>0:
+            user = get_object_or_404(TestUsersAuth,pk=id)
+        #By default render_to_response uses Context TEMPLATE_CONTEXT_PROCESSORS
+        response = render_to_response('admin/user_edit.html',{'user':user},
+            context_instance=RequestContext(request)
+        )
+    elif save == 'save':
+        if id == 0:
+            user = TestUsersAuth()
+            user_form = TestUsersAuthForm(request.POST, instance=user)
+        elif id > 0:
+            user = TestUsersAuth.objects.get(pk=id)
+            user_form = TestUsersAuthForm(request.POST, instance=user)
+        
+        
+        try:
+            #Validation happens on save
+            user_form.save()
+            
+            #if all savings are ok we will send a simple ajax response to tell the client
+            #that main page can be reloaded (by the client) to refresh the users_list
+            response = HttpResponse('Saved==True', mimetype="text/plain")
+        except Exception,e:
+            #TEMPLATE_CONTEXT_PROCESSORS are simple functions that automatically add to templates
+            #specific parameters (for ex. rel. to security) like: csrf_token
+            #RequestContext always use django.core.context_processors.csrf 
+            #render_to_response can be forced to use RequestContext context
+            user.error = e
+            response = render_to_response('admin/user_edit.html',{'user':user},
+                context_instance=RequestContext(request)
+                #,[] #here we can add context processors functions (even our custom)
+            )
+            '''
+            #only for test
+            response = HttpResponse(e)
+            '''
+            
+    else:
+        raise Http404
+    
+    #this is a response to an ajax request
+    return response
+    
+
